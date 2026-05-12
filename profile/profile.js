adb528e9a6aa479d51de1c9fb63527f04b16a05c
Khaled Mohamed <khaled.mohamed@gmail.com>
2026-05-12 14:17:31 +0300

    Last Changes

--- profile/profile.js
+++ profile/profile.js
@@ -1,10 +1,20 @@
-const user_name = document.querySelector(".user-name"); 
-window.addEventListener('DOMContentLoaded', () => {
-    const userData = localStorage.getItem("userData"); 
-    if(userData) { 
-        const user = JSON.parse(userData); 
-        user_name.textContent = user.name; 
-    } else { 
-        console.log("fail");
-    }
-});
+const user_name = document.querySelector(".user-name");
+const themeBtn = document.getElementById("themeBtn");
+
+window.onload = () => {
+  const userData = localStorage.getItem("userData");
+  if (userData) {
+    const user = JSON.parse(userData);
+    user_name.textContent = user.name;
+  } else {
+    console.log("There's Error in Naming User");
+  }
+  if (themeBtn) {
+    themeBtn.addEventListener("click", () => {
+      document.body.classList.toggle("light");
+      themeBtn.textContent = document.body.classList.contains("light")
+        ? "☀️"
+        : "🌙";
+    });
+  }
+};
